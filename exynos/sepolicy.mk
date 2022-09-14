miwu04/device_spark_sepolicy

Partially Revert "sepolicy: Deprecate LiveDisplay and Lineage FOD"
This reverts commit f887e48b48d586bec9f9a25cfd8c3a0a703e330e.

exynos/sepolicy.mk
@@ -1,0 +1,23 @@
+#
+# This policy configuration will be used by all exynos products
+# that inherit from PixelExperience
+#
+
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/spark/sepolicy/exynos/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/spark/sepolicy/exynos/dynamic \
+    device/spark/sepolicy/exynos/system
+else
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/spark/sepolicy/exynos/dynamic \
+    device/spark/sepolicy/exynos/vendor
+endif

EpicROM-AOSP/android_device_epic_sepolicy

Signed-off-by: Escobar1945 <candaroglu197878@gmail.com>

exynos/sepolicy.mk
@@ -1,0 +1,23 @@
+#
+# This policy configuration will be used by all exynos products
+# that inherit from PixelExperience
+#
+
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/epic/sepolicy/exynos/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/epic/sepolicy/exynos/dynamic \
+    device/epic/sepolicy/exynos/system
+else
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/epic/sepolicy/exynos/dynamic \
+    device/epic/sepolicy/exynos/vendor
+endif